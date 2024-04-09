38831c0c8221a89d7422339d2c2d04f4045ed028
Ruman1234567 <ruman.ali@student.vonknh.nl>
2024-04-09 09:30:28 +0200

    dd

---
+++ files/Login/index.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+require "login.php";
+?>
+
+<main>
+    <div class="wrapper-main">
+        <section class="section-default">
+            <?php
+if (isset($_SESSION['userId'])) {
+echo '<p class="login-status"> You are logged in!</p>';
+}
+else {
+    echo '<p class="login-status"> You are logged out!</p>';
+}
+            ?>
+</section>
+    </div>
+</main>
+
+<?php 
+require "footer.php";
+?>
