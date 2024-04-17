ca278f66de1da6c8b192f2c3aa8bfe4a8df755d2
Ruman1234567 <ruman.ali@student.vonknh.nl>
2024-04-17 13:34:45 +0200

    Update header.php

--- files/header.php
+++ files/header.php
@@ -1,11 +1,19 @@
+<!-- header.php -->
+
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="description" content="this is in example of an meta description. this will often show in search results">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LogIn Sherm</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
 <header>
@@ -14,36 +22,31 @@
             <img src="" alt="logo">    
         </a>
         <ul>
-            <li><a href="index.php" class="nav-button">Home</a></li>
-            <li><a href="#" class="nav-button">Ho</a></li>
-            <li><a href="#" class="nav-button">Contact</a></li>
-            <li><a href="#" class="nav-button">About Me</a></li>
-            <li>
-                <?php
-                if (isset($_SESSION['userId'])) {
-                    echo '<form action="./includes/logout.inc.php" method="post">
-                            <button type="submit" name="Logout-submit" class="logout-button">Logout</button>
-                          </form>';
-                } else {
-                    echo '<a href="signup.php" class="signup-button">Signup</a>';
-                }
-                ?>
-            </li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Ho</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About Me</a></li>
+            <?php
+            // Check if user is logged in
+            if (isset($_SESSION['userId'])) {
+                echo '<li><a href="profile.php">Profile</a></li>'; // Add Profile button
+            }
+            ?>
         </ul>
     </nav>
     <div class="header-login">
         <?php
         if (isset($_SESSION['userId'])) {
-            echo '<form action="./includes/logout.inc.php" method="post">
-                    <button type="submit" name="Logout-submit" class="logout-button">Logout</button>
-                  </form>';
+            echo ' <form action="./includes/logout.inc.php" method="post">
+                    <button type="submit" name="Logout-submit">Logout</button>
+                </form>';
         } else {
             echo '<form action="./includes/login.inc.php" method="post">
                     <input type="text" name="mailuid" placeholder="Username/E-mail...">
                     <input type="password" name="pwd" placeholder="Password...">
-                    <button type="submit" name="Login-submit" class="login-button">Login</button>
-                  </form>
-                  <a href="signup.php" class="signup-button">Signup</a>';
+                    <button type="submit" name="Login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>';
         }
         ?>
     </div>
